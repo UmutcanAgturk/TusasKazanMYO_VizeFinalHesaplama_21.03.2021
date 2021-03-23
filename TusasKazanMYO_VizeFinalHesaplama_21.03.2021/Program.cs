adf745b5c91101a13a0d7566c73232dd1f0378a8
Umutcan Ağtürk <76958190+UmutcanAgturk@users.noreply.github.com>
2021-03-23 10:16:18 +0300

    1.1

--- TusasKazanMYO_VizeFinalHesaplama_21.03.2021/Program.cs
+++ TusasKazanMYO_VizeFinalHesaplama_21.03.2021/Program.cs
@@ -37,54 +37,76 @@ namespace TusasKazanMYO_VizeFinalHesaplama_21._03._2021
 
                 if (final < 50)//Eğer finalden kalınırsa oromatik olarak ff ile geçirilmeyecektir 
                 {
-                    Console.Write("Öğrenci Adı:"+ogrenci+" Ortalamanız: " + ortalama + "(Harf Notunuz FF) Final Sınavından 50 Barajını Geçemediniz İçin Kaldınız.");
+                    Console.Write("Öğrenci Adı:"+ogrenci+" Ortalamanız: " + ortalama + " (Harf Notunuz FF) Final Sınavından 50 Barajını Geçemediniz İçin Kaldınız.");
                     // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                    Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
                 }
                 else
                 {
                     //Matematiksel işlemlerden sonra oratalama not verilen değerler arasındaysa o kod aktif ediliyor. 
                     if (ortalama >= 92 && ortalama <= 100)
                     {
-                        Console.Write("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + "(Harf Notunuz AA) Dersten Pekiyi Derecede Geçtiniz");
+                        Console.Write("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + " (Harf Notunuz AA) Dersten Pekiyi Derecede Geçtiniz");
                         // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
                     }
                     else if (ortalama >= 82 && ortalama <= 91)
                     {
-                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + "(Harf Notunuz BA) Bu Dersten İyi-Pekiyi Derecede Geçtiniz");
+                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + " (Harf Notunuz BA) Bu Dersten İyi-Pekiyi Derecede Geçtiniz");
                         // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
                     }
                     else if (ortalama >= 73 && ortalama <= 81)
                     {
-                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + "(Harf Notunuz BB) Bu Dersten İyi Derecede Geçtiniz");
+                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + " (Harf Notunuz BB) Bu Dersten İyi Derecede Geçtiniz");
                         // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
                     }
-                    else if (ortalama >= 64 && ortalama <= 73)
+                    else if (ortalama >= 65 && ortalama <= 72)
                     {
-                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + "(Harf Notunuz CB) Bu Dersten Orta-İyi Derecede Geçtiniz");
+                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + " (Harf Notunuz CB) Bu Dersten Orta-İyi Derecede Geçtiniz");
                         // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
                     }
                     else if (ortalama >= 55 && ortalama <= 64)
                     {
-                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + "(Harf Notunuz CC) Bu Dersten Orta Derecede Geçtiniz");
+                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + " (Harf Notunuz CC) Bu Dersten Orta Derecede Geçtiniz");
                         // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
                     }
-                    else if (ortalama >= 48 && ortalama <= 55)
+                    else if (ortalama >= 48 && ortalama <= 54)
                     {
-                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + "(Harf Notunuz DC) Bu Dersten Şartlı Olarak Geçtiniz");
+                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + " (Harf Notunuz DC) Bu Dersten Şartlı Olarak Geçtiniz");
                         // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
 
                     }
-                    else if (ortalama >= 40 && ortalama <= 48)
+                    else if (ortalama >= 40 && ortalama <= 47)
                     {
-                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + "(Harf Notunuz DD) Bu Dersten Şartlı Olarak Geçtiniiz");
+                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + " (Harf Notunuz DD) Bu Dersten Şartlı Olarak Geçtiniiz");
                         // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
                     }
-                    else if (ortalama >= 0 && ortalama <= 40)
+                    else if (ortalama >= 0 && ortalama <= 39)
                     {
-                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + "(Harf Notunuz FF) Bu Dersten Kaldınız");
+                        Console.WriteLine("Öğrenci Adı:" + ogrenci + " Ortalamanız: " + ortalama + " (Harf Notunuz FF) Bu Dersten Kaldınız");
                         // 'ogrenci'= isimleri direk yazdırıyoruz // 'ortalama' = yaptığımız matematik işleminin verisini vermektedir.
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
+                    }
+
+                    if (vize < 0 || vize > 100)
+                    {
+                        Console.WriteLine("Geçersiz Not");
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
+                    }
+
+                    else if (final < 0 || final > 100)
+                    {
+                        Console.WriteLine("Geçersiz Not");
+                        Console.WriteLine("Devam etmek istiyorsanız enter tuşuna basınız");
                     }
                 }
+                
                 mevcut[i] = Console.ReadLine();
                 
             }
